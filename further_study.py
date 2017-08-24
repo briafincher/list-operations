5a6461a45f6aaf76de982bb651a97720370f3b6c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-24 14:56:23 -0700

    made custom insert function

--- further_study.py
+++ further_study.py
@@ -100,7 +100,7 @@ def custom_insert(input_list, index, value):
 
     """
 
-    pass
+    input_list[index-1:index+1] = [input_list[index-1], value, input_list[index]]
 
 
 def custom_remove(input_list, value):
